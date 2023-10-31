a428a172bfbd674025d0b452dcf8d19751eb27d8
charlesreign <charlesgold45@gmail.com>
2023-10-31 22:24:07 +0000

    created responsive mobile view menus

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import {useState} from 'react'
 import styles from './Header.module.scss'
 import { Link } from 'react-router-dom'
 import {FaCartShopping} from 'react-icons/fa6'
+import {HiOutlineMenuAlt3} from 'react-icons/hi'
+import { FaTimes } from 'react-icons/fa'
 
 const logo = (
   <div className={styles.logo}>
@@ -23,17 +25,38 @@ const cart = (
   </span>
 )
 
+
+
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false)
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu)
+  }
+
+  const hideMenu = () => {
+    setShowMenu(false)
+  }
+
+
   return (
     <header>
       <div className={styles.header}>
         {logo}
-        <nav>
-          <ul>
+        <nav className={showMenu ? `${styles['show-nav']}` : `${styles['hide-nav']}`}>
+          <div className={showMenu ? `${styles['nav-wrapper']} ${styles['show-nav-wrapper']}` : `${'nav-wrapper'}`} onClick={hideMenu}></div>
+
+          <ul onClick={hideMenu}>
+            <li className={styles["logo-mobile"]}>
+              <Link to="/">
+                {logo}
+              </Link>
+              <FaTimes size={22} color='#fff' onClick={hideMenu}/>
+            </li>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/contact">Contact Us</Link></li>
           </ul>
-          <div className={styles["header-right"]}>
+          <div className={styles["header-right"]} onClick={hideMenu}>
             <span className={styles.links}>
               <Link to='/login'>Login</Link>
               <Link to='/register'>Register </Link>
@@ -42,6 +65,11 @@ const Header = () => {
             {cart}
           </div>
         </nav>
+        
+        <div className={styles["menu-icon"]}>
+          {cart}
+          <HiOutlineMenuAlt3 size={28} onClick={toggleMenu}/>
+        </div>
       </div>
     </header>
   )
