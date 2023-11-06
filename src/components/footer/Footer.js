b094ec35d896c4848a562706ad3adf530d87a816
charlesreign <charlesgold45@gmail.com>
2023-11-06 21:20:38 +0000

    added styles to footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import styles from './Footer.module.scss'
+
+const date = new Date()
+const year = date.getFullYear()
+
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className={styles.footer}>
+      &copy; {year} All Rights Reserved
     </div>
   )
 }
