38b03a5f7159b045c53a8089832eb5ae08f548de
charlesreign <charlesgold45@gmail.com>
2023-11-06 22:05:47 +0000

    created and added code to login component

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,10 +1,31 @@
 import React from 'react'
-
+import styles from './auth.module.scss'
+import loginImg from '../../assets/login.png'
+import { Link } from 'react-router-dom'
+import {FaGoogle} from 'react-icons/fa'
 const Login = () => {
     return (
-        <div>
-        
-        </div>
+        <section className={`container ${styles.auth}`}>
+            <div className={styles.img}>
+                <img src={loginImg} alt="Login" width='400px'/>
+            </div>
+            <div className={styles.form}>
+                <h2>Login</h2>
+                <form>
+                    <input type="text" placeholder='Email' required/>
+                    <input type="password" placeholder='Password' required />
+                    <button className='--btn --btn-primary --btn-block'>Login</button>
+                    <div className={styles.links}>
+                        <Link to='/reset'>Reset Password</Link>
+                    </div>
+                    <p>-- or --</p>
+                </form>
+                <button className='--btn --btn-danger --btn-block'><FaGoogle/> Login With Google</button>
+                <span className={styles.register}>
+                    <p>Don't have an account? <Link to='/register'>Register</Link></p>
+                </span>
+            </div>
+        </section>
     )
 }
 
