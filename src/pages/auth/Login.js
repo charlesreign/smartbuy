a3521fbe3cf8e4706d420f4ebdc8468d9c2e3b29
charlesreign <charlesgold45@gmail.com>
2023-12-04 23:49:05 +0000

    added code for signin with email and password

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,35 +1,94 @@
-import React from 'react'
-import styles from './auth.module.scss'
-import loginImg from '../../assets/login.png'
-import { Link } from 'react-router-dom'
-import {FaGoogle} from 'react-icons/fa'
-import Card from '../../components/card/Card'
-const Login = () => {
-    return (
-        <section className={`container ${styles.auth}`}>
-            <div className={styles.img}>
-                <img src={loginImg} alt="Login" width='400px'/>
-            </div>
-            <Card>
-                <div className={styles.form}>
-                    <h2>Login</h2>
-                    <form>
-                        <input type="text" placeholder='Email' required/>
-                        <input type="password" placeholder='Password' required />
-                        <button className='--btn --btn-primary --btn-block'>Login</button>
-                        <div className={styles.links}>
-                            <Link to='/reset'>Reset Password</Link>
-                        </div>
-                        <p>-- or --</p>
-                    </form>
-                    <button className='--btn --btn-danger --btn-block'><FaGoogle/> Login With Google</button>
-                    <span className={styles.register}>
-                        <p>Don't have an account? <Link to='/register'>Register</Link></p>
-                    </span>
-                </div>
-            </Card>
-        </section>
-    )
-}
+import { useState } from "react";
+import styles from "./auth.module.scss";
+import loginImg from "../../assets/login.png";
+import { Link } from "react-router-dom";
+import { FaGoogle } from "react-icons/fa";
+import Card from "../../components/card/Card";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase/config";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useNavigate } from "react-router-dom";
+import Loader from "../../components/loader/Loader";
 
-export default Login
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const loginUser = (e) => {
+    e.preventDefault();
+
+    setIsLoading(true);
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+
+        console.log(user);
+
+        setIsLoading(false);
+
+        toast.success("Login successful");
+
+        navigate("/");
+      })
+      .catch((error) => {
+        setIsLoading(false);
+
+        toast.error(
+          `An error has occurred: ${error.message} with error code: ${error.code}`
+        );
+      });
+  };
+
+  return (
+    <>
+      <ToastContainer />
+      {isLoading && <Loader />}
+      <section className={`container ${styles.auth}`}>
+        <div className={styles.img}>
+          <img src={loginImg} alt="Login" width="400px" />
+        </div>
+        <Card>
+          <div className={styles.form}>
+            <h2>Login</h2>
+            <form onSubmit={loginUser}>
+              <input
+                type="text"
+                placeholder="Email"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button className="--btn --btn-primary --btn-block">Login</button>
+              <div className={styles.links}>
+                <Link to="/reset">Reset Password</Link>
+              </div>
+              <p>-- or --</p>
+            </form>
+            <button type="submit" className="--btn --btn-danger --btn-block">
+              <FaGoogle /> Login With Google
+            </button>
+            <span className={styles.register}>
+              <p>
+                Don't have an account? <Link to="/register">Register</Link>
+              </p>
+            </span>
+          </div>
+        </Card>
+      </section>
+    </>
+  );
+};
+
+export default Login;
