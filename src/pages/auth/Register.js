68b0d946dc6be0052b7eaaf8ba2ac34f24b49695
charlesreign <charlesgold45@gmail.com>
2023-12-04 22:55:25 +0000

    user registration phase 1

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,31 +1,61 @@
-import React from 'react'
-import styles from './auth.module.scss'
-import registerImg from '../../assets/register.png'
-import Card from '../../components/card/Card'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import styles from "./auth.module.scss";
+import registerImg from "../../assets/register.png";
+import Card from "../../components/card/Card";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-    return (
-        <section className={`container ${styles.auth}`}>
-            <Card>
-                <div className={styles.form}>
-                    <h2>Register</h2>
-                    <form>
-                        <input type="email" placeholder='Email' required/>
-                        <input type="password" placeholder='Password' required />
-                        <input type="password" placeholder='Confirm Password' required />
-                        <button className='--btn --btn-primary --btn-block'>Register</button>
-                    </form>
-                    <span className={styles.register}>
-                        <p>Already have an account? <Link to='/login'>Login</Link></p>
-                    </span>
-                </div>
-            </Card>
-            <div className={styles.img}>
-                <img src={registerImg} alt="Register" width='400px'/>
-            </div>
-        </section>
-    )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [cPassword, setCPassword] = useState("");
 
-export default Register
+  const registerUser = (e) => {
+    e.preventDefault();
+    console.log(email, password, cPassword);
+  };
+  return (
+    <section className={`container ${styles.auth}`}>
+      <Card>
+        <div className={styles.form}>
+          <h2>Register</h2>
+          <form onSubmit={registerUser}>
+            <input
+              type="email"
+              placeholder="Email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              required
+              value={cPassword}
+              onChange={(e) => setCPassword(e.target.value)}
+            />
+            <button className="--btn --btn-primary --btn-block" type="submit">
+              Register
+            </button>
+          </form>
+          <span className={styles.register}>
+            <p>
+              Already have an account? <Link to="/login">Login</Link>
+            </p>
+          </span>
+        </div>
+      </Card>
+      <div className={styles.img}>
+        <img src={registerImg} alt="Register" width="400px" />
+      </div>
+    </section>
+  );
+};
+
+export default Register;
