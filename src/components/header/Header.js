bd5e9acd468d73e67d3b7bd71434d816d2ecbbcf
charlesreign <charlesgold45@gmail.com>
2023-12-05 00:03:39 +0000

    added code to logout

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,14 @@
-import {useState} from 'react'
-import styles from './Header.module.scss'
-import { Link, NavLink } from 'react-router-dom'
-import {FaCartShopping} from 'react-icons/fa6'
-import {HiOutlineMenuAlt3} from 'react-icons/hi'
-import { FaTimes } from 'react-icons/fa'
+import { useState } from "react";
+import styles from "./Header.module.scss";
+import { Link, NavLink } from "react-router-dom";
+import { FaCartShopping } from "react-icons/fa6";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import { FaTimes } from "react-icons/fa";
+import { signOut } from "firebase/auth";
+import { auth } from "../../firebase/config";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import Loader from "../loader/Loader";
 
 const logo = (
   <div className={styles.logo}>
@@ -13,66 +18,114 @@ const logo = (
       </h2>
     </Link>
   </div>
-)
+);
 
 const cart = (
   <span className={styles.cart}>
-    <Link to='/cart'>
+    <Link to="/cart">
       Cart
-      <FaCartShopping size={20}/>
+      <FaCartShopping size={20} />
       <p>{0}</p>
-      </Link>
+    </Link>
   </span>
-)
+);
 
-const activeLink = ({isActive})=>(isActive ? `${styles.active}`:"")
+const activeLink = ({ isActive }) => (isActive ? `${styles.active}` : "");
 
 const Header = () => {
-  const [showMenu, setShowMenu] = useState(false)
+  const [showMenu, setShowMenu] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
 
   const toggleMenu = () => {
-    setShowMenu(!showMenu)
-  }
+    setShowMenu(!showMenu);
+  };
 
   const hideMenu = () => {
-    setShowMenu(false)
-  }
+    setShowMenu(false);
+  };
 
+  const logoutUser = () => {
+    setIsLoading(true);
+
+    signOut(auth)
+      .then(() => {
+        toast.success("logout successful");
+        navigate("/");
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+
+        toast.error(
+          `An error has occurred: ${error.message} with error code: ${error.code}`
+        );
+      });
+  };
 
   return (
-    <header>
-      <div className={styles.header}>
-        {logo}
-        <nav className={showMenu ? `${styles['show-nav']}` : `${styles['hide-nav']}`}>
-          <div className={showMenu ? `${styles['nav-wrapper']} ${styles['show-nav-wrapper']}` : `${'nav-wrapper'}`} onClick={hideMenu}></div>
+    <>
+      {isLoading && <Loader />}
+      <header>
+        <div className={styles.header}>
+          {logo}
+          <nav
+            className={
+              showMenu ? `${styles["show-nav"]}` : `${styles["hide-nav"]}`
+            }
+          >
+            <div
+              className={
+                showMenu
+                  ? `${styles["nav-wrapper"]} ${styles["show-nav-wrapper"]}`
+                  : `${"nav-wrapper"}`
+              }
+              onClick={hideMenu}
+            ></div>
 
-          <ul onClick={hideMenu}>
-            <li className={styles["logo-mobile"]}>
-              <Link to="/">
-                {logo}
-              </Link>
-              <FaTimes size={22} color='#fff' onClick={hideMenu}/>
-            </li>
-            <li><NavLink to="/" className={activeLink}>Home</NavLink></li>
-            <li><NavLink to="/contact" className={activeLink}>Contact Us</NavLink></li>
-          </ul>
-          <div className={styles["header-right"]} onClick={hideMenu}>
-            <span className={styles.links}>
-              <NavLink to='/login' className={activeLink}>Login</NavLink>
-              <NavLink to='/register' className={activeLink}>Register </NavLink>
-              <NavLink to='/order-history' className={activeLink}>My Orders</NavLink>
-            </span>
+            <ul onClick={hideMenu}>
+              <li className={styles["logo-mobile"]}>
+                <Link to="/">{logo}</Link>
+                <FaTimes size={22} color="#fff" onClick={hideMenu} />
+              </li>
+              <li>
+                <NavLink to="/" className={activeLink}>
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/contact" className={activeLink}>
+                  Contact Us
+                </NavLink>
+              </li>
+            </ul>
+            <div className={styles["header-right"]} onClick={hideMenu}>
+              <span className={styles.links}>
+                <NavLink to="/login" className={activeLink}>
+                  Login
+                </NavLink>
+                <NavLink to="/register" className={activeLink}>
+                  Register{" "}
+                </NavLink>
+                <NavLink to="/order-history" className={activeLink}>
+                  My Orders
+                </NavLink>
+                <NavLink to="/" onClick={logoutUser}>
+                  logout
+                </NavLink>
+              </span>
+              {cart}
+            </div>
+          </nav>
+
+          <div className={styles["menu-icon"]}>
             {cart}
+            <HiOutlineMenuAlt3 size={28} onClick={toggleMenu} />
           </div>
-        </nav>
-        
-        <div className={styles["menu-icon"]}>
-          {cart}
-          <HiOutlineMenuAlt3 size={28} onClick={toggleMenu}/>
         </div>
-      </div>
-    </header>
-  )
-}
+      </header>
+    </>
+  );
+};
 
-export default Header
+export default Header;
